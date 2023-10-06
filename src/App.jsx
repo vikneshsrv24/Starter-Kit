f03f1046a04328e9699296b76f79eeaa0b990c0b
SRV <97838319+vikneshsrv24@users.noreply.github.com>
2023-10-06 15:09:01 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+      <div className='bg-black flex justify-center items-center w-full h-screen text-white text-3xl tracking-wider'>
+        This is a starter kit. Edit <span className='text-blue-300 px-2'>App.jsx</span> for your project.
+      </div>
+  )
+}
+
+export default App
